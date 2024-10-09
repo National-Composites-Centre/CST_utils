f2d1e1bbeb958248776c0a75712b40230e377b80
Jakub Kucera <Jakub.Kucera@nccuk.com>
2024-10-09 17:11:11 +0100

    baseline tolerance store

--- ApplyTolerances/AT.py
+++ ApplyTolerances/AT.py
@@ -1,6 +1,10 @@
 import CompositeStandard as cs
 from CompositeStandard import *
 from pydantic import BaseModel
+import importlib
+from utils import reLink
+from STL.file_utils import clean_json
+from jsonic import serialize, deserialize
 #apply tolerances
 
 
@@ -53,15 +57,6 @@ print(toll)
 
 
 
-#call this function to store specific tolerance
-def StoreTol(D,tolerance,sp_obj=None):
-    #D main CompoST object
-    #tolerance is the single tolerance object
-    #sp_obj is used where the tolerance is specified to take effect for specific object rather than full part based on relimitation
-
-    return()
-
-
 #TKINTER UI
 import tkinter as tk
 from tkinter import ttk
@@ -74,6 +69,118 @@ class TolLine(BaseModel):#
     delete_button: Optional[object] = Field(None)
     value_button: Optional[object] = Field(None)
     tol_obj: Optional[object] = Field(None)
+    var_inputs: Optional[list[object]] = Field(None)
+
+def SaveTols(D,yp_list):
+
+    #assign ID!
+    ID = D.fileMetadata.maxID
+    
+    if D.allTolerances == None:
+        D.allTolerances = []
+
+    for tol in yp_list:
+        ID = ID + 1
+        tol.tol_obj.ID = ID
+        D.allTolerances.append(tol.tol_obj)
+
+    #save max ID
+    D.fileMetadata.maxID = ID
+
+        #turn data back to JSON
+    json_str = serialize(D, string_output = True)
+
+    #clean the JSON
+    json_str = clean_json(json_str)
+
+    #save the JSON
+    #save as file
+    print("saving as:",path+"\\"+filename+".json")
+    with open(path+"\\"+filename+"_.tolsjson", 'w') as out_file:
+        out_file.write(json_str)
+    return(D)
+
+def SaveOBJ(yP3_var,yp_list,subWin):
+
+    #add relimitation
+    
+    yP3 = yP3_var.get()
+    
+    #TODO keep ignores the same, or find a diff. mechanic for saving below
+    ignore = ["stageID","deactivate_stageID","additionalParameters","additionalProperties",
+                        "ID","axisSystemID","splineRelimitation","splineRelimitationRef","active"]
+
+    for o in yp_list: 
+        if o.ref_pos == yP3:
+            #loop through all attributes
+            i = 0
+            for at in o.tol_obj.__dict__:
+
+                if at not in ignore:
+
+                    print(o.var_inputs, "o.var_inputs")
+                    setattr(o.tol_obj,at,o.var_inputs[i].get())
+                    
+                    i += 1
+
+    subWin.destroy()
+
+    #print(o.tol_obj)
+    
+def DefineTol(event,yP3_var,yp_list):
+
+    yP3 = yP3_var.get()
+    for o in yp_list: 
+        if o.ref_pos == yP3:
+            
+            atrs = o.tol_obj.__dict__
+            #print(atrs)
+
+
+            # create app
+            subWin = tk.Tk()
+            subWin.title("Specify Parameters")
+            subWin.geometry("510x610")
+
+
+            my_frame = Frame(subWin, width=500, height=600) 
+            my_frame.pack() # Note the parentheses added here
+
+            subWin.resizable(True,True)
+
+            posy = 10
+            posx = 5
+
+            #ignore these attributes
+            ignore = ["stageID","deactivate_stageID","additionalParameters","additionalProperties",
+                        "ID","axisSystemID","splineRelimitation","splineRelimitationRef","active"]
+            #TODO axisSystemID selection add - interactive?
+            #TODO spliner relims to be done internactively 
+
+            o.var_inputs = []
+            #loop through all attributes
+            for at in atrs:
+                #print(at)
+                #create name and input field for each attribute
+                if at not in ignore:
+                    l = ttk.Label(subWin, text = at,width = 80)
+                    l.place(x=posx,y=posy)
+
+                    it = ttk.Entry(subWin,width = 40,text = atrs[at] )
+                    it.place(x=posx+160,y=posy)
+                    posy += 30
+
+                    o.var_inputs.append(it)
+                    
+            #create button that submits the info and closes the subWindow
+            button = ttk.Button(my_frame,text="Save",command= lambda: SaveOBJ(yP_var,yp_list,subWin))
+            button.place(x=60,y=posy)
+
+            subWin.mainloop()
+
+            
+            #print("x")
+
 
 def DeleteTol(event,yP3_var,yp_list):
 
@@ -88,20 +195,20 @@ def DeleteTol(event,yP3_var,yp_list):
             #delete from list
             yp_list.remove(o)
 
-    print("x")
 
 
 def CreateTwo(event,yP2_var,yp_list):
 
     yP2 = yP2_var.get()
-    print(yp_list)
+    class_selected = event.widget.get()
+    #print(yp_list)
     for o in yp_list:
         if o.ref_pos == yP2:
             if o.delete_button == None:
 
                 yP3_var = IntVar(value=yP2)
 
-                button1 = ttk.Button(my_frame,text="Define")
+                button1 = ttk.Button(my_frame,text="Define",command= lambda: DefineTol(event,yP3_var,yp_list))
                 button1.place(x=400,y=yP2)
                 button2 = ttk.Button(my_frame,text="Delete",command= lambda: DeleteTol(event,yP3_var,yp_list))  
                 button2.place(x=490,y=yP2)
@@ -109,12 +216,20 @@ def CreateTwo(event,yP2_var,yp_list):
                 o.delete_button = button2
                 o.value_button = button1
 
-            
+            #regardless of weather buttons needed generating
+            module_name = "CompositeStandard"
+            module = importlib.import_module(module_name)
+            class_name = class_selected.strip("<>").split("'")[1].split('.', 1)[1]
+            class_ = getattr(module,class_name)
+            o.tol_obj = class_()
+
+
+
 
 def AddTolLine(yP_var,yp_list):
 
     yP = yP_var.get()
-    yP = yP + 20
+    yP = yP + 30
     yP_var.set(yP)
 
     yP2_var = IntVar(value=yP)
@@ -125,22 +240,33 @@ def AddTolLine(yP_var,yp_list):
     #create the object
     t = TolLine(main_button = combo,ref_pos=yP)
     yp_list.append(t)
-    button.place(x=20,y=button.winfo_y()+20)
-    print(yp_list)
+    button.place(x=20,y=button.winfo_y()+30)
+    buttonS.place(x=20,y=buttonS.winfo_y()+30)
+    #print(yp_list)
 
- 
+
+
+path = "D:\\CAD_library_sampling\\CompoST_examples\\orientation_map_example"
+filename = "sq_test_001"
+with open(path+"\\"+filename+"_layup.json","r") as in_file:
+    json_str= in_file.read()
+
+#turn file into workable classes
+D = deserialize(json_str,string_input=True)
+
+#re-link - if relevant
+D = reLink(D)
+
 
 # create app
 root = tk.Tk()
-root.title("xxx")
-root.geometry("710x710+150+150")
-
+root.title("Tolerance definitions")
+root.geometry("610x610+150+150")
 
 yP_var = IntVar(value=0)
 yp_list = []
 
-
-my_frame = Frame(root, width=700, height=700) 
+my_frame = Frame(root, width=700, height=600) 
 my_frame.pack() # Note the parentheses added here
 
 root.resizable(True,True)
@@ -148,7 +274,8 @@ root.resizable(True,True)
 button = ttk.Button(my_frame,text="Add Tolerance Definition",command= lambda: AddTolLine(yP_var,yp_list))
 button.place(x=20,y=50)
 
-
+buttonS = ttk.Button(my_frame,text="Save All",command = lambda: SaveTols(D,yp_list))
+buttonS.place(x=20,y=80)
 
 root.mainloop()
 
