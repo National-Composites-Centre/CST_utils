d3da38f7e1970780063db4abf47705d1130ce42a
Jakub Kucera <Jakub.Kucera@nccuk.com>
2025-02-13 14:23:12 +0000

    mesh

--- utils.py
+++ utils.py
@@ -221,3 +221,24 @@ def reLink(D):
     return(D)
 
 
+def clean_json(strin):
+    #strin = input json string to clean
+    s = strin.replace("{","\n{\n")
+    s = s.replace("}","\n}\n")
+
+
+    tabs = 0
+    new_str = ""
+    for line in s.split("\n")[:]:
+        
+        if "}" in line:
+            tabs = tabs - 1
+
+        for ii in range(0,tabs):
+            new_str += "   "
+        new_str += line+"\n"
+
+        if "{" in line:
+            tabs = tabs + 1
+    #returns a human readable JSON
+    return(new_str)
